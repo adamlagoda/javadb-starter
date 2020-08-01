478b58b46b872a56045a1e9a87dff797cfced096
adamlagoda <adam_lagoda@interia.pl>
2020-08-01 14:54:54 +0200

    Implementacja findUser

--- library-manager/src/main/java/org/example/library/users/JdbcUsersDao.java
+++ library-manager/src/main/java/org/example/library/users/JdbcUsersDao.java
@@ -1,12 +1,41 @@
 package org.example.library.users;
 
+import org.example.jdbc.starter.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcUsersDao implements IUsersDao {
 
+    private static final String DATABASE_PROPERTIES = "library-database.properties";
+    private static final Logger logger = LoggerFactory.getLogger(JdbcUsersDao.class);
+    private ConnectionFactory connectionFactory;
+
+    public JdbcUsersDao() {
+        connectionFactory = new ConnectionFactory(DATABASE_PROPERTIES)
+    }
+
     @Override
     public User findUser(String login, String password) {
+        logger.info("Finding user by login = " + login + " and password = " + password);
+        try (Connection connection = connectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE login = ? AND password = ?")) {
+            statement.setString(1, login);
+            statement.setString(2, password);
+            ResultSet userData = statement.executeQuery();
+            if (userData.next()) {
+                return new User(userData.getInt("id"), userData.getString("login"), userData.getString("password"),
+                        userData.getString("name"), userData.getBoolean("admin"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
