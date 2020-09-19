0a0399d4187983a0fefc75fc88a5eff9edf77a7e
adamlagoda <adam_lagoda@interia.pl>
2020-09-19 12:30:02 +0200

    ConnectionFactory cleanup

--- jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
+++ jdbc-starter/src/main/java/org/example/jdbc/starter/ConnectionFactory.java
@@ -13,10 +13,10 @@ import java.util.Properties;
 public class ConnectionFactory {
     private static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
 
-    private final Properties properties;
+    //private final Properties properties;
 
     public ConnectionFactory(String filename) {
-        this.properties = getDataBaseProperties(filename);
+        /*this.properties = getDataBaseProperties(filename);*/
     }
 
     private Properties getDataBaseProperties(String filename) {
@@ -46,15 +46,15 @@ public class ConnectionFactory {
         MysqlDataSource dataSource = null;
         try {
             dataSource = new MysqlDataSource();
-            dataSource.setServerName(properties.getProperty("org.example.jdbc.starter.server"));
-            dataSource.setDatabaseName(properties.getProperty("org.example.jdbc.starter.name"));
-            dataSource.setUser(properties.getProperty("org.example.jdbc.starter.user"));
-            dataSource.setPassword(properties.getProperty("org.example.jdbc.starter.password"));
-            dataSource.setPort(Integer.valueOf(properties.getProperty("org.example.jdbc.starter.port")));
-            dataSource.setServerTimezone("Europe/Warsaw");
-            dataSource.setAllowMultiQueries(true);
+            dataSource.setServerName();
+            dataSource.setDatabaseName();
+            dataSource.setUser();
+            dataSource.setPassword();
+            dataSource.setPort();
+            dataSource.setServerTimezone();
+            /*dataSource.setAllowMultiQueries(true);
             dataSource.setAllowPublicKeyRetrieval(true);
-            dataSource.setUseSSL(false);
+            dataSource.setUseSSL(false);*/
             dataSource.setCharacterEncoding("UTF-8");
         } catch (SQLException e) {
             logger.error("Error during creating MysqlDataSource", e);
